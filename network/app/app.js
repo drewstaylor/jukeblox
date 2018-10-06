e7bbeea3071b157d22b415127e36b27e64a931b1
Drew Taylor <drew@infiniteinternet.ca>
2018-10-06 15:23:35 -0700

    ignore mp3

--- network/app/app.js
+++ network/app/app.js
@@ -48,6 +48,8 @@ app.use(function(err, req, res, next) {
 
   // Return API error message
   errMsg = toErrorMsg(errMsg);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.send(errMsg);
 });
 
