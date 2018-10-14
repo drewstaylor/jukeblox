e27c8d9770797e67615670446f0a0da1cc05afbb
Drew Taylor <drew@infiniteinternet.ca>
2018-10-14 16:38:12 -0400

    port normalization

--- network/app/app.js
+++ network/app/app.js
@@ -7,6 +7,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+const http = require('http');
+const https = require('https');
+
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -84,5 +87,28 @@ var toErrorMsg = function (errMsg) {
 };
 
 app.use(errorHandler);
+
+// Certificate
+//const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+//const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+//const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+/*const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};*/
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+/*httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});*/
 
 module.exports = app;
