4fedf5d7de6f90da329a6489120c33718583c3ca
Chris Lackey <chris@infiniteinternet.ca>
2018-11-26 17:34:43 -0500

    IN PROGRESS: testing gun db and song library migration

--- app/src/app/services/library.service.ts
+++ app/src/app/services/library.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subscriber, throwError } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
+import Gun from 'gun';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,14 @@ import { Observable, Subscriber, throwError } from 'rxjs';
 export class LibraryService {
 
   public library: Array<any>;
+  private gun: any;
 
   constructor(private http: HttpClient) {
+    // XXX; replace this later with server / node URL
+    const gunPeers = ['https://jukeblox-gun.herokuapp.com/gun'];
+    this.gun = Gun(gunPeers);
+    window['gun'] = this.gun;
+
     this.fetchLibrary();
   }
 
@@ -19,10 +26,43 @@ export class LibraryService {
       .toPromise()
       .then((library: string) => {
         this.library = Array.from(library);
+        // this.gunMigrate(this.library);
+
       })
       .catch((error: Error) => {
         console.error(error);
       });
+  }
+
+
+  private gunMigrate(library: Array<any>): void {
+    const songs = this.gun.get('songs');
+
+    library.forEach(song => {
+      const title = song.title || null;
+      const artist = song.artist || null;
+      const album = song.album || null;
+      const trackNumber = song.trackNumber || null;
+      const index = song.index;
+      const tags = song.tags;
+
+      // Create the gun entry for the song
+      const songRef = this.gun.get(song.index);
+      const tagRef = songRef.get('tags');
+
+      songRef.get('title').put(title);
+      songRef.get('artist').put(artist);
+      songRef.get('album').put(album);
+      songRef.get('trackNumber').put(trackNumber);
+      songRef.get('index').put(index);
+
+      // Convert tag array to object (Gun doesn't accept arrays);
+      tags.forEach(tag => {
+        tagRef.set(tag);
+      });
+
+      songs.set(songRef);
+    });
   }
 
 
@@ -32,14 +72,14 @@ export class LibraryService {
     } else {
       // Search these three fields
       const searchKeys = ['title', 'artist', 'album'];
-      var result = this.library.filter((song: any) => {
+      const result = this.library.filter((song: any) => {
         // Try each field if available
         for (const key of searchKeys) {
           if (song.hasOwnProperty(key)) {
             const needle = queryString.toLowerCase();
             // Factor out punctuation
             const haystack = song[key].replace(/([^\w\s]|_)/gi, '').toLowerCase();
-            
+
             if (haystack.includes(needle)) {
               return true;
             }
